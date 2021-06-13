5b3363e45e796eb95363001229b392f3bb5bb341
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2021-06-14 01:09:45 +0900

    urlsをアプリケーションに移動

---
+++ django_app/hello/urls.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 14 00:40:07 2021
+
+@author: owner
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    ]
