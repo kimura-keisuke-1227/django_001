9656780afcf18771a1f4974391c90a81772f94af
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2021-06-14 15:15:11 +0900

    2−3　P.111 コメント追記

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -5,16 +5,19 @@ from .forms import HelloForm
 # Create your views here.
 
 class HelloView(TemplateView):
+    #初期化処理
     def __init__(self):
         self.params = {
             'title'     :'Hello',
             'message'   :'your data',
             'form'      :HelloForm()
             }
-    
+        
+    #GETときの処理
     def get(self,request):
         return render(request, 'hello/index.html',self.params)
     
+    #POST時の処理
     def post(self,request):
         msg = 'あなたは、<br>' + request.POST['name'] + 'さんです。' + \
             "<br>" + "メールアドレスは" + "<b>" + \
