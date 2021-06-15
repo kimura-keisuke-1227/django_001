2645abb55e2e0c43eb97c907971a042c2c921330
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2021-06-15 15:53:17 +0900

    3-3 P.177 id指定でデータ表示にトライ。 ボタン押したらエラー

--- django_app/hello/forms.py
+++ django_app/hello/forms.py
@@ -9,7 +9,8 @@ Created on Mon Jun 14 09:05:55 2021
 from django import forms
 
 class HelloForm(forms.Form):
-    check = forms.BooleanField(label="CheckBox" , required=False)
+    id = forms.IntegerField(label='ID')
+    #check = forms.BooleanField(label="CheckBox" , required=False)
     #name = forms.CharField(label='name')
     #mail = forms.CharField(label='mail')
     #age = forms.IntegerField(label='age')
